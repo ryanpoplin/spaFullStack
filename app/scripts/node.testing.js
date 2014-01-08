ff63d05e340025013df9f4b9c736ef748028276e
Ryan Poplin <mobileupstate@gmail.com>
2014-01-08 15:26:33 -0500

    Advanced JS concepts for Pro SPA and Node, yo...

--- app/scripts/node.testing.js
+++ app/scripts/node.testing.js
@@ -1,3 +1,4 @@
+// it's the bigger picture dude/ette
 'use strict';
 (function () {
 	var 
@@ -24,4 +25,76 @@
 	counter.increment();
 	counter.increment();
 	counter.removeListener('incremented', callback);
+	// engine testing
+	var
+		testOne
+	;
+	testOne = 'Just testing here...';
+	function testFuncOne (testOne) {
+		// var hoisting occured, but was not init over first pass init
+		console.log(testOne);
+		var testOne;
+		console.log(testOne);
+	}
+	testFuncOne(testOne);
+	// 'super important' === 'execution context and the execution context object'
+	// === empty context obj
+	outerCon(23);
+	// arg === declared && init
+	// global execution context
+	function outerCon (arg) {
+		// local var declared && === undefined
+		// init while executing
+		// scope contexts
+		var 
+			localVar
+		;
+		localVar = 'it\'s just a local thing, you wouldn\'t understand';
+		function innerCon () {
+			console.log('innerCon');
+		}
+		// no execution on first passing
+		// a new context object 
+		innerCon();
+	}
+	// prototype-based testing === sexy
+	// 1st
+	var
+		objectCreate,
+		proto,
+		MakeProgrammer,
+		firstProgrammer,
+		secondProgrammer
+	;
+	objectCreate = function (arg) {
+		if (!arg) { return  {}; }
+		function obj () {};
+		obj.prototype = arg;
+		return new obj;
+	};
+	Object.create = Object.create || objectCreate;
+	// prototype framework
+	// just like defaults
+	proto = {
+		name: 'Neck Beard',
+		age: 33,
+		languages: ['HTML5', 'CSS3', 'JavaScript']
+	};
+	// constructor function/object
+	MakeProgrammer = function (name, age) {
+		var
+			programmer
+		;
+		programmer = Object.create(proto);
+		programmer.name = name;
+		programmer.age = age;
+		return programmer;
+	};
+	// instantiate
+	firstProgrammer = MakeProgrammer('Ryan Poplin', 23);
+	secondProgrammer = MakeProgrammer('Electric Dodds', 27);
+	// object instances
+	console.log(firstProgrammer);
+	console.log(secondProgrammer);
+	
 }());
